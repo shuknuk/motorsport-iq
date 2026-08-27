484cd8e39de26a446c83d3d207c4d24362eaa590
shuknuk <kg467@outlook.com>
2026-08-26 21:21:23 -0400

    Activate audio session asynchronously

--- MotorsportIQ/SoundService.swift
+++ MotorsportIQ/SoundService.swift
@@ -1,4 +1,5 @@
 import AVFoundation
+import AVFAudio
 
 enum RaceSound: String, Sendable {
     case questionAlert = "question-alert"
@@ -8,6 +9,7 @@ enum RaceSound: String, Sendable {
 
 @MainActor
 final class SoundService {
+    private let audioSession = AVAudioSession.sharedInstance()
     private var players: [RaceSound: AVAudioPlayer] = [:]
 
     init() {
@@ -19,6 +21,18 @@ final class SoundService {
 
     func play(_ sound: RaceSound) {
         guard UserDefaults.standard.object(forKey: "msp_sounds_enabled") as? Bool ?? true else { return }
-        players[sound]?.currentTime = 0; players[sound]?.play()
+        guard let player = players[sound] else { return }
+        if #available(iOS 27.0, *) {
+            audioSession.activate(options: []) { [weak player] activated, _ in
+                guard activated else { return }
+                Task { @MainActor [weak player] in
+                    player?.currentTime = 0
+                    player?.play()
+                }
+            }
+        } else {
+            player.currentTime = 0
+            player.play()
+        }
     }
 }
